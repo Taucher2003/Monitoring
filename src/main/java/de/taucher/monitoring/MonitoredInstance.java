4555c78d87c58c3a69206e1103986a17b5ccba45
Taucher <mc.taucher2003@gmail.com>
2020-07-21 23:04:00 +0200

    Some changes suggested by Dieter

--- src/main/java/de/taucher/monitoring/MonitoredInstance.java
+++ src/main/java/de/taucher/monitoring/MonitoredInstance.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Timer;
@@ -12,129 +11,120 @@ import java.util.TimerTask;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 
 public class MonitoredInstance {
+
+	private static final String OK_SYMBOL = "<a:vrox_yes:716334571708481578>";
+	private static final String BAD_SYMBOL = OK_SYMBOL; //Symbol coming soon
+	private static final String FAIL_SYMBOL = "<a:vrox_no:716334571515412482>";
+	private static final String[] StateNames = new String[] { "Is reachable", "Bad connections", "Not reachable" };
 
 	private String name;
 	private String displayname;
 	private String host;
 	private int port;
 	private MessageChannel[] channels;
-	private int wasReachable = 0;
-	
-	private EmbedBuilder reach;
-	private EmbedBuilder bad;
-	private EmbedBuilder unreach;
-	
+	private int wasReachable = -1;
+
+	EmbedBuilder embeds[];
+
 	private SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy, HH:mm:ss", Locale.GERMANY);
-	
+
 	MonitoredInstance(String name, String displayname, String host, int port, MessageChannel... notify) {
 		this.name = name;
 		this.displayname = displayname;
 		this.host = host;
 		this.port = port;
 		this.channels = notify;
-		
-		this.reach = new EmbedBuilder().setDescription("**Serverstate changed**\r\n" + 
-				"\r\n" + 
-				"<a:vrox_yes:716334571708481578> **"+name+"** is now reachable\r\n\r\n" + 
-				":desktop: Address: `"+host+":"+port+"`").setColor(0x15d11c);
-		this.bad = new EmbedBuilder().setDescription("**Serverstate changed**\r\n" + 
-				"\r\n" + 
-				"<a:vrox_yes:716334571708481578> **"+name+"** has a bad connection\r\n\r\n" + 
-				":desktop: Address: `"+host+":"+port+"`").setColor(0xec6602);
-		this.unreach = new EmbedBuilder().setDescription("**Serverstate changed**\r\n" + 
-				"\r\n" + 
-				"<a:vrox_no:716334571515412482> **"+name+"** is no longer reachable\r\n\r\n" + 
-				":desktop: Address: `"+host+":"+port+"`").setColor(0xd11a15);
+
+		embeds = new EmbedBuilder[3];
+		embeds[0] = getEmbedBuilder("is now reachable", OK_SYMBOL, 0x15d11c);
+		embeds[1] = getEmbedBuilder("has a bad connection", BAD_SYMBOL, 0xec6602);
+		embeds[2] = getEmbedBuilder("is no longer reachable", FAIL_SYMBOL, 0xd11a15);
+
 		scheduleCheck();
 	}
-	
+
 	private void scheduleCheck() {
-		new Timer("Monitoring "+host+":"+port, true).scheduleAtFixedRate(new TimerTask() {
+		new Timer("Monitoring " + host + ":" + port, true).scheduleAtFixedRate(new TimerTask() {
 			@Override
 			public void run() {
 				check();
 			}
-		}, 1000, 60*1000);
+		}, 1000, 60 * 1000);
 	}
-	
+
 	public void check() {
-		try {
-			System.out.println("["+format.format(new Date())+"] Checking " + host+":"+port + " ("+name+")");
-			int reachable;
-			Socket socket = new Socket();
-			try {
-		        socket.connect(new InetSocketAddress(host, port), 5000);
-		        reachable = 1;
-		    }catch (IOException e) {
-		    	try {
-			    	socket.connect(new InetSocketAddress(host, port), 10000);
-			        reachable = 2;
-		    	}catch(IOException ex) {
-		    		reachable = 3;
-		    	}
-		    }finally {
-		    	socket.close();
-		    }
-			System.out.println("["+format.format(new Date())+"] Checked " + host+":"+port + " ("+name+") -> "+(reachable == 1 ? "Is reachable" : reachable == 2 ? "Bad performance" : "Not reachable"));
-			if(wasReachable != 0) {
-				if(reachable == 1 && wasReachable != reachable) {
-					Monitoring.getInstance().getMessages(this).forEach(message -> {
-						Monitoring.getInstance().updateMessage(message);
-					});
-					Arrays.asList(channels).forEach(messagechannel -> {
-						messagechannel.sendMessage(reach.setFooter("Timestamp • "+format.format(new Date())).build()).queue();
-					});
-				}
-				if(reachable == 2 && wasReachable != reachable) {
-					Monitoring.getInstance().getMessages(this).forEach(message -> {
-						Monitoring.getInstance().updateMessage(message);
-					});
-					Arrays.asList(channels).forEach(messagechannel -> {
-						messagechannel.sendMessage(bad.setFooter("Timestamp • "+format.format(new Date())).build()).queue();
-					});
-				}
-				if(reachable == 3 && wasReachable != reachable) {
-					Monitoring.getInstance().getMessages(this).forEach(message -> {
-						Monitoring.getInstance().updateMessage(message);
-					});
-					Arrays.asList(channels).forEach(messagechannel -> {
-						messagechannel.sendMessage(unreach.setFooter("Timestamp • "+format.format(new Date())).build()).queue();
-					});
-				}
-			}else {
-				Monitoring.getInstance().getMessages(this).forEach(message -> {
-					Monitoring.getInstance().updateMessage(message);
-				});
-			}
-			wasReachable = reachable;
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+
+		int state = checkSystem();
 	
+		// emit message
+		System.out.println("["+format.format(new Date())+"] Checked " + host+":"+port + " ("+name+") -> "+ StateNames[state]);
+		
+		if(wasReachable != state) {
+			Monitoring.getInstance().getMessages(this).forEach(message -> {
+				Monitoring.getInstance().updateMessage(message);
+			});
+			if(wasReachable != -1) {
+				MessageEmbed msg = embeds[state].setFooter("Timestamp • "+format.format(new Date())).build();
+				for(MessageChannel messagechannel : channels) { 
+					messagechannel.sendMessage(msg).queue();
+				}
+			}
+		}
+			
+		wasReachable = state;
+	}
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getDisplayName() {
 		return displayname;
 	}
-	
+
 	public String getHost() {
 		return host;
 	}
-	
+
 	public int getPort() {
 		return port;
 	}
-	
+
 	public MessageChannel[] getChannels() {
 		return channels;
 	}
-	
+
 	public int getStatus() {
-		return wasReachable;
+		return wasReachable + 1; // +1 to keep original semantics
 	}
+	
+	public void resetStatus() {
+		wasReachable = -1;
+	}
+
+	private EmbedBuilder getEmbedBuilder(String state, String symbol, int color) {
+		return new EmbedBuilder().setDescription("**Serverstate changed**\r\n" + "\r\n" + symbol + " **" + name + "** "
+				+ state + "\r\n\r\n" + ":desktop: Address: `" + host + ":" + port + "`").setColor(color);
+	}
+	
+	private int checkSystem() {
+		// check system state
+		System.out.println("["+format.format(new Date())+"] Checking " + host+":"+port + " ("+name+")");
+		int reachable = 0;
+		Socket socket = new Socket();
+		do {
+			try {
+		       socket.connect(new InetSocketAddress(host, port), (reachable+1) * 5000);
+		       socket.close();
+		       break;
+			}catch(IOException e) {
+		    	reachable++;
+			}
+		}while(reachable < 2);
+		return reachable;
+	}
+	
 }
--- src/main/java/de/taucher/monitoring/MonitoringMessage.java
+++ src/main/java/de/taucher/monitoring/MonitoringMessage.java
@@ -5,6 +5,10 @@ import net.dv8tion.jda.api.entities.Message;
 
 public class MonitoringMessage {
 	
+	private static final String OK_SYMBOL = "<a:vrox_yes:716334571708481578>";
+	private static final String BAD_SYMBOL = OK_SYMBOL; //Symbol coming soon
+	private static final String FAIL_SYMBOL = "<a:vrox_no:716334571515412482>";
+	
 	private String category;
 	private Message message;
 	private MonitoredInstance[] instances;
@@ -22,13 +26,13 @@ public class MonitoringMessage {
 		int color = 0x15d11c;
 		
 		for(MonitoredInstance mi : instances) {
-			String emoji = mi.getStatus() == 1 ? "<a:vrox_yes:716334571708481578>" : mi.getStatus() == 2 ? "<a:vrox_yes:716334571708481578>" : "<a:vrox_no:716334571515412482>";
+			String emoji = mi.getStatus() == 1 ? OK_SYMBOL : mi.getStatus() == 2 ? BAD_SYMBOL : FAIL_SYMBOL;
 			desc.append(emoji + " " + mi.getDisplayName() + "\n\n");
 		}
-//		if(color == 0x15d11c && desc.toString().contains("")) {
+//		if(desc.toString().contains(BAD_SYMBOL)) {
 //			color = 0xec6602;
 //		}
-		if(desc.toString().contains("<a:vrox_no:716334571515412482>")) {
+		if(desc.toString().contains(FAIL_SYMBOL)) {
 			color = 0xd11a15;
 		}
 		
