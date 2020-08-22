cdcf9a7eb5c97f394ed518398abed61304cfad52
Taucher <sdfsaf@werwet.de>
2020-08-22 14:08:38 +0200

    Update Constants.java

--- src/main/java/de/taucher/monitoring/Constants.java
+++ src/main/java/de/taucher/monitoring/Constants.java
@@ -3,4 +3,9 @@ package de.taucher.monitoring;
 public class Constants {
 
 	String botToken = "";
+	String mysqlHost = "";
+	int mysqlPort = 3306;
+	String mysqlUser = "";
+	String mysqlPass = "";
+	String mysqlDatabase = "";
 }
